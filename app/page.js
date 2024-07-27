ba3a7863862493748a224da7aa21658d311fb1f8
Anderson V <andersonventosilla0@gmail.com>
2024-07-27 10:40:30 -0500

    sidebar panel with next

--- app/page.js
+++ app/page.js
@@ -1,113 +1,79 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { ClipboardPlus, Database, Landmark, Layers3, LayoutDashboard, Settings, UserX } from 'lucide-react';
+import Link from 'next/link';
+import { ToggleButton } from '@/components/toggleButton';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex dark:bg-slate-800'>
+      <div className="min-h-screen">
+        <div className="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r hover:w-56 hover:shadow-lg">
+          <div className="flex h-screen flex-col justify-between pt-2 pb-6">
+            <div>
+              <ul className="space-y-2 tracking-wide">
+                <Link href='/' className="min-w-max bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600" >
+                  <Image src='/logofmcamps.png' alt='logo' width={26} height={30} priority />
+                  <span className="-mr-1 font-medium dark:text-white text-slate-400">Panel de control</span>
+                </Link>
+
+                <Link
+                  href='/'
+                  className="min-w-max bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600"
+                >
+                  <Image src='/logofmcamps.png' alt='logo' width={26} height={30} priority />
+                  <span className="-mr-1 dark:text-white text-slate-400">Sysadmin</span>
+                </Link>
+
+                {/* seleccionado */}
+                <Link
+                  href='/'
+                  className="min-w-max bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600"
+                >
+                  <UserX className='hover:text-cyan-300 dark:text-white text-slate-900' />
+                  <span className="-mr-1 dark:text-white text-slate-400">Dashboard</span>
+                </Link>
+                {/*  */}
+
+                <Link
+                  href='/'
+                  className="min-w-max bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600"
+                >
+                  <Layers3 className='hover:text-cyan-300 dark:text-white text-slate-900' />
+                  <span className="group-hover:text-gray-700 dark:text-white text-slate-400">Usuarios</span>
+                </Link>
+                <Link
+                  href='/'
+                  className="min-w-max group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
+                >
+                  <ClipboardPlus className='hover:text-cyan-300 dark:text-white text-slate-900' />
+                  <span className="group-hover:text-gray-700 dark:text-white text-slate-400">Operaciones</span>
+                </Link>
+                <Link
+                  href='/'
+                  className="min-w-max group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
+                >
+                  <Database className='hover:text-cyan-300 dark:text-white text-slate-900' />
+                  <span className="group-hover:text-gray-700 dark:text-white text-slate-400">Permisos</span>
+                </Link>
+                <Link
+                  href='/'
+                  className="min-w-max group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
+                >
+                  <Landmark className='hover:text-cyan-300 dark:text-white text-slate-900' />
+                  <span className="group-hover:text-gray-700 dark:text-white text-slate-400">Configuraciones</span>
+                </Link>
+              </ul>
+            </div>
+            <div className="w-max -mb-3">
+              <Link href='/' className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                <Settings className='hover:text-cyan-300 dark:text-white text-slate-900' />
+                <span className="group-hover:text-gray-700 dark:text-white text-slate-400">Settings</span>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <ToggleButton />
+    </div>
   );
 }
